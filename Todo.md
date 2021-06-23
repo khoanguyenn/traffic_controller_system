24eeb6a4bb8b90c09c0dcdc85c4d5983c74de997
phatngluu <yadsendew@protonmail.com>
2021-06-23 23:34:49 +0700

    update endpoint and todo

---
+++ Todo.md
@@ -0,0 +1,3 @@
+Below are features must be fulfilled:
+- Restrict user from viewing device which is not owned by him/her.
+- On user profile page, user can click on the device to navigate to device's stream.
--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,3 +1,6 @@
 export const Endpoint = {
     GET_VEHICLES : "https://jsonplaceholder.typicode.com/photos?_limit=10",
+
+    LIST_DEVICES : "/api/device/list-devices",
+    GET_DEVICE : "/api/device/get-device?deviceId=" // TODO need to append device id
 };
