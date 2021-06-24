bb00c17ef5ddec745e47418bfcf02386dd5c261f
phatngluu <yadsendew@protonmail.com>
2021-06-25 00:38:48 +0700

    try to use auth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,162 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import UserProfile from "./components/UserProfile/UserProfile";
 import TrafficController from "./components/TrafficController/TrafficController";
+import React, { useContext, createContext, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/home" component={TrafficController}/>
-          <Route exact path="/profile" component={UserProfile}/>
-          <Route exact path="/login"></Route>
-          <Route path="/home/device/:deviceId" children={<TrafficController />} />
-        </Switch>
-      </Router>
-    </div>
+      <ProvideAuth  className="App">
+        <Router>
+          <div>
+            <AuthButton />
+            <Switch>
+              <Route path="/login">
+                <LoginPage />
+              </Route>
+              <PrivateRoute path="/">
+                <UserProfile />
+              </PrivateRoute>
+              <PrivateRoute path="/home">
+                <TrafficController />
+              </PrivateRoute>
+              <PrivateRoute path="/home/device/:deviceId">
+                <TrafficController />
+              </PrivateRoute>
+              <PrivateRoute path="/profile">
+                <UserProfile />
+              </PrivateRoute>
+            </Switch>
+          </div>
+        </Router>
+      </ProvideAuth>
   );
 }
 
 export default App;
+
+const fakeAuth = {
+  isAuthenticated: false,
+  signin(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  },
+};
+
+const authContext = createContext();
+
+function ProvideAuth({ children }) {
+  const auth = useProvideAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+}
+
+function useAuth() {
+  return useContext(authContext);
+}
+
+function useProvideAuth() {
+  const [user, setUser] = useState(null);
+
+  const signin = (cb) => {
+    return fakeAuth.signin(() => {
+      setUser("user");
+      cb();
+    });
+  };
+
+  const signout = (cb) => {
+    return fakeAuth.signout(() => {
+      setUser(null);
+      cb();
+    });
+  };
+
+  return {
+    user,
+    signin,
+    signout,
+  };
+}
+
+function AuthButton() {
+  let history = useHistory();
+  let auth = useAuth();
+
+  return auth.user ? (
+    <p>
+      Welcome!{" "}
+      <button
+        onClick={() => {
+          auth.signout(() => history.push("/"));
+        }}
+      >
+        Sign out
+      </button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  );
+}
+
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated.
+function PrivateRoute({ children, ...rest }) {
+  let auth = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        auth.user ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function PublicPage() {
+  return <h3>Public</h3>;
+}
+
+function ProtectedPage() {
+  return <h3>Protected</h3>;
+}
+
+function LoginPage() {
+  let history = useHistory();
+  let location = useLocation();
+  let auth = useAuth();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+  let login = () => {
+    auth.signin(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <p>You must log in to view the page at {from.pathname}</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  );
+}
