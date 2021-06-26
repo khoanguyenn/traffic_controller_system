aacae53b3d30b94c3c77967399371693881bb30d
phatngluu <yadsendew@protonmail.com>
2021-06-26 14:13:07 +0700

    update endpoint

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,7 +1,7 @@
 export const Endpoint = {
     GET_VEHICLES : "https://jsonplaceholder.typicode.com/photos?_limit=10",
 
-    LIST_DEVICES : "http://localhost:4000/api/device/list-devices",
-    GET_DEVICE : "http://localhost:4000/api/device/get-device?deviceId=",
+    LIST_DEVICES : "/api/device/list-devices",
+    GET_DEVICE : "/api/device/get-device?deviceId=",
     DEVICE_SCREEN_URL: "/home/device/",
 };
